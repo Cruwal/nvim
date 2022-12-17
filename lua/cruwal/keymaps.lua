ce33e7f74264a6ce2f106983a5454329bbc5cf4d
Cruwal <wallace.souza@foxbit.com.br>
2022-12-17 14:00:47 -0300

    remove unneeded keymaps

--- lua/cruwal/keymaps.lua
+++ lua/cruwal/keymaps.lua
@@ -4,10 +4,6 @@ vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>w', '<cmd>w<CR>')
 vim.keymap.set('n', '<leader>c', '<cmd>bd<CR>')
 
---------- Visual ----------
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
-
 -- Better window navigation
 vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
