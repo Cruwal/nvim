d0e50de07a817f8138dce4c982839619a86579ce
Cruwal <wallace.souza@foxbit.com.br>
2023-01-07 19:07:48 -0300

    add keymap to gitsigns stage_hunk

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,8 +1,12 @@
-local gitsigns = require('gitsigns')
+local status_ok, gitsigns = pcall(require, 'gitsigns')
+if not status_ok then
+  return
+end
 
 gitsigns.setup()
 vim.keymap.set('n', '<leader>gj', gitsigns.next_hunk, {})
 vim.keymap.set('n', '<leader>gk', gitsigns.prev_hunk, {})
 vim.keymap.set('n', '<leader>gp', gitsigns.preview_hunk, {})
 vim.keymap.set('n', '<leader>gb', gitsigns.blame_line)
-vim.keymap.set('n', '<leader>gu', ':Gitsigns reset_hunk<CR>')
+vim.keymap.set('n', '<leader>gu', gitsigns.reset_hunk)
+vim.keymap.set('n', '<leader>ga', gitsigns.stage_hunk)
