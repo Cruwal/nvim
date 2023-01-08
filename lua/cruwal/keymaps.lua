b734b86acbc949a120275eb2bbb78ecb6a4abb3a
Cruwal <wallace.souza@foxbit.com.br>
2023-01-07 21:18:18 -0300

    add a 'go to test file' function

--- lua/cruwal/keymaps.lua
+++ lua/cruwal/keymaps.lua
@@ -26,6 +26,7 @@ vim.keymap.set('n', '<S-h>', ':bprevious<CR>')
 -- RSpec
 vim.keymap.set('n', '<leader>t', require("custom.rspec").run_test_file)
 vim.keymap.set('n', '<leader>tl', require("custom.rspec").run_current_test)
+vim.keymap.set('n', '<leader>gt', require("custom.rspec").go_to_test_file)
 
 vim.keymap.set('n', '<space>d', vim.diagnostic.open_float)
 
--- lua/custom/rspec.lua
+++ lua/custom/rspec.lua
@@ -3,12 +3,33 @@ local function run_test_file()
 end
 
 local function run_current_test()
-  local line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_win_get_cursor(0)[1]
 
   return vim.cmd("! rspec %:" .. line)
 end
 
+local function go_to_test_file()
+  local extension = vim.bo.filetype
+  if not extension == 'ruby' then
+    return
+  end
+
+  local filename = vim.fn.expand('%:t')
+  local file_head = vim.fn.expand('%:h')
+  if filename.match(filename, "_spec.rb") then
+    Formatted_extension = filename.gsub(filename, "_spec.rb", ".rb")
+    Formatted_head = file_head.gsub(file_head, "spec", "app")
+  else
+    Formatted_extension = filename.gsub(filename, ".rb", "_spec.rb")
+    Formatted_head = file_head.gsub(file_head, "app", "spec")
+  end
+
+  local command = "e " .. Formatted_head .. "/" .. Formatted_extension
+  return vim.cmd(command)
+end
+
 return {
   run_current_test = run_current_test,
-  run_test_file = run_test_file
+  run_test_file = run_test_file,
+  go_to_test_file = go_to_test_file
 }
