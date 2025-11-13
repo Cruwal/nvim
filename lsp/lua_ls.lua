bc8295f69c3fb9e1a9ecf000e321a54316257230
Cruwal <wallacecs@hotmail.com.br>
2025-11-13 12:08:55 -0300

    add LSP folder

---
+++ lsp/lua_ls.lua
@@ -0,0 +1,9 @@
+return {
+  cmd = { 'lua-language-server' },
+  filetype = { 'lua' },
+  root_markers = { '.luarc.json' },
+
+  settings = {
+    Lua = {},
+  },
+}
---
+++ lsp/ruby_lsp.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { 'ruby-lsp' },
+  filetype = { 'ruby', 'eruby' },
+  root_markers = { 'Gemfile' }
+}
