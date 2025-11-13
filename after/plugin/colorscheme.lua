9277698204c4837f7ec1a757ed201b25291b2fd7
Cruwal <wallacecs@hotmail.com.br>
2025-11-13 09:10:54 -0300

    feat: improve error colorschema error handling

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,7 +1,17 @@
 local colorscheme = "tokyonight"
 
+local ok, schema = pcall(require, colorscheme)
+if not ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
+schema.setup({
+  style = "night"
+})
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  vim.notify("Could not set the " .. colorscheme .. " colorscheme!")
   return
 end
