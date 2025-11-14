ac37edd03ba7f91a9022f21f724eb39d09762336
Cruwal <wallacecs@hotmail.com.br>
2025-11-14 12:27:20 -0300

    feat: improve LSP configuration

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,6 @@
 vim.lsp.enable({ 'ruby_lsp', 'lua_ls', 'ts_ls' })
 
-vim.keymap.set('n', '<space>d', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 
